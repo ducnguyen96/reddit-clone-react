2e437959fd74cafc52ead323053061fd2b4e5b1b
ducnguyen96 <levinguyen.dl@gmail.com>
2021-10-20 02:31:04 +0700

    update config

--- src/core/config.ts
+++ src/core/config.ts
@@ -5,7 +5,7 @@ import configs from "../config";
 
 // Get the name of the current environment from `<body data-env="...">`,
 // which is being injected by the Cloudflare Worker script.
-const envName = document.body.dataset.env || "prod";
+const envName = "prod";
 const config = configs[envName as unknown as keyof typeof configs];
 if (!config) throw new Error();
 
